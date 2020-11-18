2726b9672b25cfd26ff17438032e552d59aaa510
nicholasscabral <nicholasscabral@gmail.com>
2020-11-18 00:20:08 -0300

    setting basic routes

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from '../components/Navbar'
+
+import '../styles/Landing.css';
+
+function Landing() {
+  return (
+    <Navbar />
+
+    
+  )
+}
+
+export default Landing;
