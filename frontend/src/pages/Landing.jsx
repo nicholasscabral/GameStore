0b732d28c6b60f8153b350a5066bbbf4e0bfcbbb
nicholasscabral <nicholasscabral@gmail.com>
2020-11-19 20:52:10 -0300

    adding static banners

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,13 +1,59 @@
 import React from 'react';
 
 import Navbar from '../components/Navbar'
+import jogo from '../assets/jogo.png'
+import shoppingCart from '../assets/carrinho.svg'
 
 import '../styles/Landing.css';
 
 function Landing() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
 
+      <div id="main">
+      <div className="games-grid">
+        <li>
+          <img src={jogo} alt=""/>
+          <div className="info">
+            <p>Titulo: Star wars</p>
+            <p>Preço: R$ 200 </p>
+            <p>Ano: 2018 </p>
+            <button> <img src={shoppingCart} alt="carrinho"/> Add ao carrinho</button>
+          </div>
+        </li>
+        <li>
+          <img src="https://store-images.s-microsoft.com/image/apps.25992.13992297318455813.7f45ef4a-6ca3-4445-b6fc-9f00be7560b6.0367424f-9788-47e6-b8ea-e71067da1d53?mode=scale&q=90&h=225&w=150&background=%23FFFFFF" alt=""/>
+          <div className="info">
+            <p>Titulo: Call of Duty-Cold War</p>
+            <p>Preço: R$ 280 </p>
+            <p>Ano: 2020 </p>
+            <button><img src={shoppingCart} alt="carrinho"/> Add ao carrinho</button>
+          </div>
+        </li>
+        <li>
+          <img src={jogo} alt=""/>
+          <div className="info">
+            <p>Titulo: Star wars</p>
+            <p>Preço: R$ 200 </p>
+            <p>Ano: 2018 </p>
+            <button> <img src={shoppingCart} alt="carrinho"/> Add ao carrinho</button>
+          </div>
+        </li>
+        <li>
+          <img src="https://store-images.s-microsoft.com/image/apps.25992.13992297318455813.7f45ef4a-6ca3-4445-b6fc-9f00be7560b6.0367424f-9788-47e6-b8ea-e71067da1d53?mode=scale&q=90&h=225&w=150&background=%23FFFFFF" alt=""/>
+          <div className="info">
+            <p>Titulo: Call of Duty-Cold War</p>
+            <p>Preço: R$ 280 </p>
+            <p>Ano: 2020 </p>
+            <button><img src={shoppingCart} alt="carrinho"/> Add ao carrinho</button>
+          </div>
+        </li>
+      
+      </div>
+      </div>
+
+    </div>
     
   )
 }
