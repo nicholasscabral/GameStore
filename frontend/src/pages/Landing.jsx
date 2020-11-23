6a470a816ee937c75d5250b924479f058312948f
nicholasscabral <nicholasscabral@gmail.com>
2020-11-22 21:50:08 -0300

    get catalog from backend

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import api from '../services/api'
 
 import Navbar from '../components/Navbar'
 import jogo from '../assets/jogo.png'
@@ -8,45 +9,43 @@ import '../styles/Landing.css';
 import '../styles/Global.css'
 
 function Landing() {
+  window.onload = getCatalog
+  function getCatalog() {
+    api.get('/catalog').then(response => {
+
+      const games = response.data
+      var gamesGrid = document.querySelector('.games-grid')
+
+      games.forEach(game => {
+        var card = document.createElement('div')
+        card.setAttribute('class', 'card')
+        card.setAttribute('id', game.id)
+
+        var gameDetails = document.createElement('li')
+        gameDetails.innerHTML = `
+          <img src="${game.imgUrl}" />
+          <div class="info">
+            <p>${game.title}</p>
+            <p>Preço: R$ ${game.price}</p>
+            <p>Ano: ${game.year}</p>
+            <button><img src="${shoppingCart}" alt="carrinho"/> Adicionar ao carrinho</button>
+          </div>
+        `
+
+        card.appendChild(gameDetails)
+        gamesGrid.appendChild(card)
+      })
+
+    })
+  }
+
   return (
     <div>
       <Navbar />
 
       <div id="main">
         <div className="games-grid">
-          <div className="card">
-            <li>
-              <img src={jogo} alt=""/>
-              <div className="info">
-                <p>Titulo: Star wars</p>
-                <p>Preço: R$ 200 </p>
-                <p>Ano: 2018 </p>
-                <button> <img src={shoppingCart} alt="carrinho"/> + Carrinho</button>
-              </div>
-            </li>
-          </div>
-          <div className="card">
-            <li>
-              <img src="https://store-images.s-microsoft.com/image/apps.25992.13992297318455813.7f45ef4a-6ca3-4445-b6fc-9f00be7560b6.0367424f-9788-47e6-b8ea-e71067da1d53?mode=scale&q=90&h=225&w=150&background=%23FFFFFF" alt=""/>
-              <div className="info">
-                <p>Titulo: Call of Duty-Cold War</p>
-                <p>Preço: R$ 280 </p>
-                <p>Ano: 2020 </p>
-                <button><img src={shoppingCart} alt="carrinho"/> Adicionar ao carrinho</button>
-              </div>
-            </li>
-          </div>
-          <div className="card">
-            <li>
-              <img src={jogo} alt=""/>
-              <div className="info">
-                <p>Titulo: Star wars</p>
-                <p>Preço: R$ 200 </p>
-                <p>Ano: 2018 </p>
-                <button> <img src={shoppingCart} alt="carrinho"/> Adicionar ao carrinho</button>
-              </div>
-            </li>
-          </div>
+ 
         </div>
       </div>
 
