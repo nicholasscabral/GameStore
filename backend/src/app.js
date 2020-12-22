9720615249ec27634953ef08609c951b101ef457
nicholasscabral <nicholasscabral@gmail.com>
2020-12-22 17:53:41 -0300

    importing routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,78 +1,21 @@
+const { getCatalog, searchGame, shoppingCart, addGameToCart } = require('./routes/routes')
 const express = require('express')
-const mysql = require('mysql')
-const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-dotenv.config({ path: './.env' })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-})
+app.get('/catalog', getCatalog)
 
-db.connect( err => {
-  if (err) console.log(err)
-  else console.log("DATABASE connected")
-})
+app.get('/searchGame/:searchValue', searchGame)
 
-app.get('/catalog', (req, res) => {
-  db.query('SELECT * FROM catalog', (err, results) => {
-    if (err) return res.status(404).send("nenhum jogo encontrado")
-    else return res.status(200).send(results)
-  })
-})
+app.get('/shoppingCart', shoppingCart)
 
-app.get('/searchGame/:searchValue', (req, res) => {
-  const { searchValue } = req.params
-
-  db.query('SELECT * FROM catalog WHERE title LIKE ?', `%${searchValue}%`, (err, results) => {
-    if (err) return res.status(500).send(err)
-    else return res.status(200).send({ success: true, results: results })
-  })
-})
-
-app.get('/getGame/:id', (req, res) => {
-  const id = req.params.id
-  
-  db.query('SELECT * FROM catalog WHERE id = ?', id, (err, result) => {
-    if (err) return res.status(500).send(err)
-    else return res.status(200).send(result)
-  })
-})
-
-// ROTA EM MANUTENÇÃO 
-app.get('/shoppingCart', (req, res) => {
-  db.query('SELECT * FROM cart', (err, results) => {
-    if (err) return res.status(500).send(err)
-    else return res.status(200).send(results)
-  })
-})
-
-app.post('/addCart/:id', (req, res) => {
-  const id = req.params.id
-
-  db.query('SELECT * FROM catalog WHERE id = ?', id, (err, result) => {
-    if (err) res.status(400).send({ success: false })
-    else {
-      const gameId = result[0].id
-
-      db.query('INSERT INTO cart SET ?', {gameId: gameId}, (err, result) => {
-        if (err) {
-          console.log({error: err})
-        }
-        else return res.status(200).send({ success: true })
-      })
-    }    
-  })
-})
+app.post('/addCart/:id', addGameToCart)
 
 app.listen(4000, () => {
   console.log("API running...")
