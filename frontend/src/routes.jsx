0701624ab0d8ee3fa968dbd3140117311c0e6c78
nicholasscabral <nicholasscabral@gmail.com>
2021-01-06 16:47:45 -0300

    add admin page/route

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Landing from './pages/Landing'
 import ShoppingCart from './pages/ShoppingCart'
+import Login from './pages/Login'
+import Admin from './pages/Admin'
 
 function Routes() {
   return (
@@ -10,6 +12,9 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/shoppingcart" exact component={ShoppingCart} />
+
+        <Route path="/admin-login" component={Login} />
+        <Route path="/admin-portal" component={Admin} />
       </Switch>
     </BrowserRouter>
   )
