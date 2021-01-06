65701dd241cf2460dc9ed65983c1c5813fafa776
nicholasscabral <nicholasscabral@gmail.com>
2021-01-06 17:43:16 -0300

    refactor cart (useState, useEffect)

--- frontend/src/pages/ShoppingCart.jsx
+++ frontend/src/pages/ShoppingCart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../services/api';
 
 import Navbar from '../components/Navbar'
@@ -7,66 +7,32 @@ import '../styles/Shoppingcart.css'
 import '../styles/Global.css'
 
 function ShoppingCart() {
-  window.onload = getShoppingCart
-
-  function removeGame(event) {
-    const gameId = event.target.getAttribute('id')
-    console.log(event.target)
-    console.log(gameId)
-
+  const [games, setGames] = useState([])
+  const [totalPrice, setTotalPrice] = useState(0)
+  
+  function removeGameFromCart(gameId) {
     api.post('/deleteFromCart/' + gameId).then(response => {
       const success = response.data.success
-
+      
       if (success) window.location.reload()
     })
   }
-
+  
   function getShoppingCart() {
     api.get('/shoppingCart').then(response =>{
-      console.log(response)
-
-      var games = response.data
-      var selectedGames = document.querySelector('.selected-games')
-      var bill = document.querySelector('.prices')
-      var payment = document.querySelector('.payment')
-      var totalPrice = 0
-
+      const games = response.data
+      setGames(games)
+      
       games.forEach(game => {
-
-          totalPrice += Number(game.price)
-
-          var pricetag = document.createElement('p')
-          pricetag.setAttribute('class', 'price')
-          pricetag.innerHTML = `${game.title} R$ ${game.price}`
-
-          var card = document.createElement('div')
-          card.setAttribute('class', 'card')
-          card.setAttribute('id', game.id)
-
-          var gameDetails = document.createElement('li')
-          gameDetails.innerHTML = `
-            <img src="${game.imgUrl}" />
-            <p> ${game.title}</p>
-            <p>R$ ${game.price}</p>
-            <button id="${game.id}"> Remover </button>
-          `
-          bill.appendChild(pricetag)
-          card.appendChild(gameDetails)
-          selectedGames.appendChild(card)
-    
-      })
-
-      payment.innerHTML = ` <hr>
-        <p id="total">Total: R$ ${totalPrice} </p>
-        <button>Finalizar compra</button>
-      `
-    }).then(() => {
-      document.querySelectorAll("button").forEach(button => {
-        button.addEventListener('click', removeGame)
+        setTotalPrice(prevValue => prevValue + Number(game.price))
       })
     })
   }
 
+  useEffect(() => {
+    getShoppingCart()
+  }, [])
+  
   return (
     <div>
       <Navbar />
@@ -74,21 +40,36 @@ function ShoppingCart() {
       <div id="main">
         <div className="selected-games">
           <h1>Seu carrinho de compras </h1>
+          {games.map(game => (
+            <div className="card" id={game.id}>
+              <li>
+                <img src={game.imgUrl} />
+                <p> {game.title}</p>
+                <p>R$ {game.price}</p>
+                <button id={game.id} onClick={() => {removeGameFromCart(game.id)}}> Remover </button>
+              </li>
+            </div>
+          ))}
 
         </div>
         <div className="shopping-cart">
           <h1>Subtotal</h1>
           <div className="subtotal">
             <div className="prices">
-
+              {games.map(game => (
+                <p>{game.title} : R$ {game.price}</p>
+              ))}
             </div>
+
             <div className="payment">
-
+              <hr/>
+              <p id="total">Total: R$ {totalPrice} </p>
+              <button>Finalizar compra</button>
             </div>
+
           </div>
         </div>
       </div>
-      {/* <footer>Made with coffee by <a href=""> Nicholas cabral </a></footer> */}
 
     </div>
   )
