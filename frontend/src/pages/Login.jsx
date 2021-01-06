418bfe18bbb728fa6f3dc1a3b92d607d6b12d6fb
nicholasscabral <nicholasscabral@gmail.com>
2021-01-06 16:48:33 -0300

    auth login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom'
 
 import '../styles/Global.css'
 import '../styles/Login.css'
+import api from '../services/api'
 
 import logo from '../assets/logo.svg'
 
 function LoginPage() {
+
+  const [username, setUsername] = useState()
+  const [password, setpassword] = useState()
+  const [successfulLogin, setSuccessfulLogin] = useState(false)
+
+  async function handleSubmit() {
+    api.post('/admin-auth', {username: username, password: password}).then((response) => {
+      const success = response.data.success
+
+      if (success) setSuccessfulLogin(true)
+    })
+  }
+
   return (
-    <div id="container">
+    successfulLogin? <Redirect to="Admin-portal"/> : (
+      <div id="container">
 
-      <nav>
-        <img src={logo} alt=""/>
-        <h3>Admin-Portal</h3>
-      </nav>
+        <nav>
+          <img src={logo} alt=""/>
+          <h3>Admin-Portal</h3>
+        </nav>
 
-      <div className="login-form">
-        <form action="">
-          <div className="username-field">
-            <label htmlFor="username">Username:</label>
-            <input type="text" name="username" id="username" placeholder="Username"/>
-          </div>
-          <div className="password-field">
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" id="password" placeholder="Password"/>
-          </div>
-          <input type="submit" value="Submit"/>
-        </form>
+        <div className="login-form">
+          <form onSubmit={e => {e.preventDefault(); handleSubmit()}}>
+            <div className="username-field">
+              <label htmlFor="username">Username:</label>
+              <input type="text" onChange={e => {setUsername(e.target.value)}} name="username" id="username" placeholder="Username"/>
+            </div>
+            <div className="password-field">
+              <label htmlFor="password">Password:</label>
+              <input type="password" onChange={e => {setpassword(e.target.value)}} name="password" id="password" placeholder="Password"/>
+            </div>
+            <input type="submit" value="Submit"/>
+          </form>
+        </div>
       </div>
-    </div>
+    )
   )
 }
 
